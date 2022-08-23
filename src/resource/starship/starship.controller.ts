44f4b75be8e691344abc8c246ef456697cd747f4
Frans Billquist <frans.billquist@gmail.com>
2022-08-24 01:42:23 +0200

    write some logics for controller, ignore .vscode

--- src/resource/starship/starship.controller.ts
+++ src/resource/starship/starship.controller.ts
@@ -1,7 +1,13 @@
 import { Response, Request } from "express";
+import { starships } from "./starship.type";
+
 
 export const getAllStarships = (req: Request, res: Response) => {
-    res.status(200).json([]);
+    if(starships.length <= 0) {
+        res.status(404).send('No starships found');
+    } else {
+    res.status(200).json(starships);
+    }
 }
 
 export const getStarshipById = (req: Request, res: Response) => {
