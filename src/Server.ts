03fefc8755b6af0527ba785127c2bb32fa79ef88
Frans Billquist <frans.billquist@gmail.com>
2022-08-23 11:16:19 +0200

    Make a commit to confirmed deleted files

--- src/Server.ts
+++ src/Server.ts
@@ -1,7 +1,13 @@
 import express from 'express';
+import boosterRouter from './resource/booster/booster.router';
 
 const app = express();
 const port = 3000;
+
+app.use(express.json());
+app.use('/api/booster', boosterRouter);
+
+
 app.listen(port, () => {
   console.log(`Server is currently running on  http://localhost:${port}`);
 
--- src/resource/booster/booster.type.ts
+++
@@ -1,9 +0,0 @@
-interface Booster {
-    id: number;
-    codeName: string;
-    rud: boolean;
-    numberOfStaticFires: number;
-    testDate: Date;
-    scraped: boolean;
-}
-export const boosters: Booster[] = [];
