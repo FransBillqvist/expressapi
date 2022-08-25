c0fac92fa867ec4b18c7929e480df541a8daf1a3
Frans Billquist <frans.billquist@gmail.com>
2022-08-25 22:25:11 +0200

    Remove the array moved it to controller

--- src/resource/starship/starship.type.ts
+++ src/resource/starship/starship.type.ts
@@ -1,15 +1,13 @@
 export interface Starship {
-    id: number;
-    codeName: string;
-    rud: boolean;
-    numberOfStaticFires: number;
-    testDate: Date;
-    hop: string;
-    scraped: boolean;
-    monument: boolean;
-    orbit: string;
-    owner: string;
-    location: string;
+  id: number;
+  codeName: string;
+  rud: boolean;
+  numberOfStaticFires: number;
+  testDate: Date;
+  hop: string;
+  scraped: boolean;
+  monument: boolean;
+  orbit: string;
+  owner: string;
+  location: string;
 }
-
-export const starships: Starship[] = [];
