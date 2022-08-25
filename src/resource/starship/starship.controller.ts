14149220cfe27cc73f2c576dc19a08d93cb07632
Frans Billquist <frans.billquist@gmail.com>
2022-08-25 16:42:23 +0200

    Added pattern for json to obejct convertion

--- src/resource/starship/starship.controller.ts
+++ src/resource/starship/starship.controller.ts
@@ -1,5 +1,5 @@
 import { Response, Request } from 'express';
-import { starships } from './starship.type';
+import { starships, Starship } from './starship.type';
 
 export const getAllStarships = (req: Request, res: Response) => {
   if (starships.length <= 0) {
@@ -20,7 +20,22 @@ export const getStarshipById = (req: Request, res: Response) => {
 };
 
 export const postNewStarship = (req: Request, res: Response) => {
-  res.status(201).json({});
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  let newStarship: Starship = {
+    id: req.body.id,
+    codeName: req.body.codeName,
+    rud: req.body.rud,
+    numberOfStaticFires: req.body.numberOfStaticFires,
+    testDate: req.body.testDate,
+    hop: req.body.hop,
+    scraped: req.body.scraped,
+    monument: req.body.monument,
+    orbit: req.body.orbit,
+    owner: req.body.owner,
+    location: req.body.location,
+  };
+  starships.push(newStarship);
+  res.status(201).json(newStarship);
 };
 
 export const updateStarshipById = (req: Request, res: Response) => {
