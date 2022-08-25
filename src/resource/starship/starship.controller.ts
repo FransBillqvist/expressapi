4f2114c3b2d195c9068591ef0a5caf7347f5e699
Frans Billquist <frans.billquist@gmail.com>
2022-08-25 22:22:14 +0200

    Done all thing for grade G

--- src/resource/starship/starship.controller.ts
+++ src/resource/starship/starship.controller.ts
@@ -1,8 +1,61 @@
 import { Response, Request } from 'express';
-import { starships, Starship } from './starship.type';
+import { Starship } from './starship.type';
+
+let starships: Starship[] = [{
+    "id": 1,
+    "codeName": "Starhopper",
+    "rud": false,
+    "numberOfStaticFires": 2,
+    "testDate": new Date(2019,08,27,22,0,0,0),
+    "hop": "150M",
+    "scraped": false,
+    "monument": false,
+    "orbit": "nope",
+    "owner": "SpaceX",
+    "location": "Starbase OLM as a watertank, Boca Chica, TX, USA, Earth"
+}, {
+    "id": 2,
+    "codeName": "SN8",
+    "rud": true,
+    "numberOfStaticFires": 4,
+    "testDate": new Date(2020,12,09,22,45,0,0),
+    "hop": "12.5KM",
+    "scraped": true,
+    "monument": false,
+    "orbit": "Nope",
+    "owner": "SpaceX",
+    "location": "Starbase, Boca Chica, TX, USA, Earth"
+}, 
+{
+    "id": 3,
+    "codeName": "SN11",
+    "rud": true,
+    "numberOfStaticFires": 3,
+    "testDate": new Date(2021,3,30,13,0,0,0),
+    "hop": "10KM",
+    "scraped": true,
+    "monument": false,
+    "orbit": "nope",
+    "owner": "SpaceX",
+    "location": "Starbase, Boca Chica, TX, USA, Earth"
+},
+{
+    "id": 4,
+    "codeName": "Glenn, Mr Copy cat Pesos",
+    "rud": true,
+    "numberOfStaticFires": 1,
+    "testDate": new Date(2022,4,18,16,0,0,0),
+    "hop": "100KM",
+    "scraped": true,
+    "monument": false,
+    "orbit": "nope",
+    "owner": "Blue Origin #MrCopyCat",
+    "location": "Van Horn, TX, USA, Earth"
+}];
 
 export const getAllStarships = (req: Request, res: Response) => {
-  if (starships.length <= 0) {
+ 
+    if (starships.length <= 0) {
     res.status(404).send('No starships found');
   } else {
     res.status(200).json(starships);
@@ -39,13 +92,48 @@ export const postNewStarship = (req: Request, res: Response) => {
 };
 
 export const updateStarshipById = (req: Request, res: Response) => {
-  res.status(200).json([]);
+    let starship = starships.find(
+    (starship) => starship.id === parseInt(req.params.id)
+  );
+  if(!starship) {
+    res.status(404).send(`No starship to update with Id ${req.params.id} was found`);
+  }
+  if(starship){
+  starships.splice(starships.indexOf(starship), 1);
+  }
+  starship = {
+    id: req.body.id,
+    codeName: req.body.codeName,
+    rud: req.body.rud,
+    numberOfStaticFires: req.body.numberOfStaticFires,
+    testDate: req.body.testDate,
+    hop: req.body.hop,
+    scraped: req.body.scraped,
+    monument: req.body.monument,
+    orbit: req.body.orbit,
+    owner: req.body.owner,
+    location: req.body.location,
+  };
+  starships.push(starship);
+  res.status(200).json(starship);
 };
 
 export const deleteAllStarship = (req: Request, res: Response) => {
+  console.log('?deleteAllStarship?');
+  starships = new Array<Starship>()
   res.status(200).json(null);
 };
 
 export const deleteStarshipById = (req: Request, res: Response) => {
-  res.status(200).json(null);
+  console.log('hallåååå eller?');
+  const starship = starships.find(
+    (starship) => starship.id === parseInt(req.params.id)
+  );
+  if (!starship) {
+    res.status(404).send(`No starship with id ${req.params.id} was found`);
+  }
+  if (starship) {
+    starships.splice(starships.indexOf(starship), 1);
+    res.status(200).json(starship);
+  }
 };
